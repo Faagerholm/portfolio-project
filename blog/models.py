01fec13f283a4c7639d3da5574c8dc4c9a4c2988
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2018-04-02 13:24:35 +0300

    new clean commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_short(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title

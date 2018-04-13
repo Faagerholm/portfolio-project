4b8b322aa67de4996c42fbcb38cfaba4d4f72d67
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2018-04-13 15:02:10 +0300

    homepage main components done

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Job(models.Model):
+    title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.TextField(max_length=300)
+    achieved_at = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def pretty_date(self):
+        return self.achieved_at.strftime("%b, %Y")
